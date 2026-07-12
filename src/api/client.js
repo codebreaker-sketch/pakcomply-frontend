148ea7d95fc40cedb4c3a2aeb92bb88a1f7e4907
codebreaker-sketch <khanx2864@gmail.com>
2026-07-11 23:47:28 -0700

    Update axios client base URL to use environment variable

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const client = axios.create({ baseURL: '/api' })
+const client = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE
+})
 
 // Attach JWT on every request
 client.interceptors.request.use((config) => {
